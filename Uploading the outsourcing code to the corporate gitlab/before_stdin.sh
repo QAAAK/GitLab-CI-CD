c8283d11b40ca63ef532b07aed46234d15b2804b
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-11-05 17:42:36 +0300

    Delete Uploading the outsourcing code to the corporate gitlab directory

--- Uploading the outsourcing code to the corporate gitlab/before_stdin.sh
+++
@@ -1,9 +0,0 @@
-!# bin/bash
-
-chmod +x up_code_git.sh
-crontab -e
-#
-#
-#
-#
-#  5 * * * * /home/user/up_code_git.sh
--- Uploading the outsourcing code to the corporate gitlab/up_code_git.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-cd ./source/
-
-git switch target_branch #checkout
-git pull
-git push -u origin master -o merge_request.create
-
